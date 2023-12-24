55ffe5a0f0391ab9714d776b388c52742be1590c
punithguptha <sai@Sais-MacBook-Air.local>
2023-12-25 02:25:17 +0530

    Addition of Pagination Support

--- src/components/News.js
+++ src/components/News.js
@@ -9,17 +9,48 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page:1,
+      totalArticles:0,
+      lastPage:1
     };
+    this.pageSize=15;
     // console.log("Inside News Class Constructor");
+  }
+
+  async fetchDataForPage(pageNumber) {
+    let apiUrl = `https://newsapi.org/v2/top-headlines?apiKey=e3d9358097584900842dae7d52d7906b&country=in&category=general&page=${pageNumber}&pageSize=${this.pageSize}`;
+    let response = await fetch(apiUrl);
+    let parsedData = await response.json();
+    let lastPageNum=Math.ceil(parsedData.totalResults/this.pageSize);
+    console.log(parsedData);
+    this.setState({
+        totalArticles:parsedData.totalResults,
+        articles:parsedData.articles,
+        lastPage:lastPageNum
+    });
   }
 
   async componentDidMount() {
     console.log("Inside Component Did Mount");
-    let apiUrl = "https://newsapi.org/v2/top-headlines?apiKey=e3d9358097584900842dae7d52d7906b&country=in&category=general";
-    let response = await fetch(apiUrl);
-    let parsedData = await response.json();
-    console.log(parsedData);
-    this.setState({articles:parsedData.articles});
+    this.fetchDataForPage(this.state.page);
+  }
+
+  handlePrevClick=async ()=>{
+    let currentPage=this.state.page;
+    if(currentPage!==1){
+        this.setState({page:currentPage-1});
+        this.fetchDataForPage(currentPage-1);
+    }
+    console.log("Inside PrevClick"+ this.state.page);
+  }
+
+  handleNextClick=async ()=>{
+    let currentPage=this.state.page;
+    if(currentPage+1 <= this.state.lastPage){
+        this.setState({page:currentPage+1});
+        this.fetchDataForPage(currentPage+1);
+    }
+    console.log("Inside NextClick"+ this.state.page);
   }
 
   render() {
@@ -36,6 +67,10 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button type="button" disabled={this.state.page===1?true:false} onClick={this.handlePrevClick} className="btn btn-primary">&laquo; Previous</button>
+        <button type="button" disabled={this.state.page===this.state.lastPage?true:false} onClick={this.handleNextClick} className="btn btn-primary">Next &raquo;</button>
         </div>
       </div>
     );
