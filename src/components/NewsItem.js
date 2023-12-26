1798562593068e06555b28bab5d8ec4f7795b973
punithguptha <sai@Sais-MacBook-Air.local>
2023-12-27 00:06:00 +0530

    Correcting Badges on cards

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,9 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:1,left:'90%'}}>{source}</span>
+            <div className="position-absolute" style={{display:"flex",right:0,justifyContent:"flex-end"}}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+            </div>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}
